e863149f2c917238ce907b4888c4d05098257d4f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-10 17:37:47 -0800

    Sketch brute force solution

--- Equal Numbers Array.js
+++ Equal Numbers Array.js
@@ -1,2 +1,6 @@
 // Given a numbers array find out how many moves would take to make all numbers equal.
 // You can only change n-1 number at a time. N = numbers.length
+
+// Iterate through the array and increase all but the highest numbers by 1.
+// Do so until all numbers are equal.
+// Return the number of times you performed the change.
