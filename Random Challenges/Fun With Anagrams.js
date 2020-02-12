e4cfaf1ad528197ca82c68f3e58052778bdd6f6b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-11 18:22:12 -0800

    Add challenge description

---
+++ Random Challenges/Fun With Anagrams.js
@@ -0,0 +1,19 @@
+// Fun With Anagrams
+
+// Two strings are anagrams if they are permutations of each other.
+// In other words, both stirngs have the same size and the same characters.
+// For example, "aaagmnrs" is an anagram of "anagrams". Given an array of strings, remove
+// each stirng that is an anagram of an earlier string, then retur nthe remaining array in 
+// sorted order.
+
+// Example:
+
+// str = [code ,doce, ecod, framer, frame]
+
+// code and doce are anagrams. Remove "doce" from the array and keep the first occurance of "code" in the array.
+// code and ecod are anagrams. Do the same as above.
+// code an framer are not anagrams. Keep both stirngs in the array.
+// framer and frame are not anagrams due to the extra "r" in framer. Keep both strings in the array.
+
+// result is [code, framer, frame]
+
