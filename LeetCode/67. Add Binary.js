eb21951f99a1617eb3fb60bd1f9b1fb27eb7e355
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-21 14:52:12 -0800

    Add extra solution

--- LeetCode/67. Add Binary.js
+++ LeetCode/67. Add Binary.js
@@ -50,3 +50,18 @@ var addBinary = function(a, b) {
 
     return sum.reverse().map(v => v.toString()).join("");
 };
+
+// One proposed solution.
+var addBinary = function(a, b) {
+    var result = "";
+    var i = a.length - 1;
+    var j = b.length - 1;
+    var carry = 0;
+    while (i >= 0 || j >= 0 || carry > 0) {
+        carry += i >= 0 ? parseInt(a[i--]) : 0;
+        carry += j >= 0 ? parseInt(b[j--]) : 0;
+        result = carry % 2 + result;
+        carry = parseInt(carry / 2);
+    }
+    return result;
+};
