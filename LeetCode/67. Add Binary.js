c37c5742a5526353411942c6eaba988d21350ce8
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-21 14:47:19 -0800

    Solve challenge

--- LeetCode/67. Add Binary.js
+++ LeetCode/67. Add Binary.js
@@ -18,39 +18,35 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    // return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
     let valueA = a.split("").map(c => parseInt(c));
     let valueB = b.split("").map(c => parseInt(c));
     let sum = [];
     let bitRoll = false;
-    while(valueA.length > 0 && valueB.length > 0) {
-        let currentBit = valueA.pop() + valueB.pop();
+    
+    while(valueA.length > 0 || valueB.length > 0) {
+        let bitA = valueA.length > 0 ? parseInt(valueA.pop()) : 0;
+        let bitB = valueB.length > 0 ? parseInt(valueB.pop()) : 0;
+        let currentBit = bitA + bitB;
+        
         if (bitRoll) { ++currentBit; bitRoll = false; }
+        
         switch (currentBit) {
             case 3:
                 sum.push(1);
+                bitRoll = true;
                 break;
+                
             case 2:
                 sum.push(0);
                 bitRoll = true;
                 break;
+                
             default:
                 sum.push(currentBit);
         }
     }
-    if (bitRoll) {
-        let nextBit = 0;
-        valueA.length ? nextBit = valueA.pop() : nextBit = valueB.pop();
-        nextBit ? sum.push(0, 1) : sum.push(1);
-    }
-    else if (valueA.length > 0 || valueB.length > 0) {
-        valueA.length ? sum.push(valueA.pop()) : sum.push(valueB.pop());
-        
-    }
-    else sum.concat(valueA.reverse(), valueB.reverse()); 
     
-    console.log(sum);
+    if (bitRoll) sum.push(1);
+
     return sum.reverse().map(v => v.toString()).join("");
 };
-
-// This approach is not working on the case of "1" and "1111".... Over complicated solution, needs to be redesigned.
