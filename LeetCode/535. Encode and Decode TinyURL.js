020464153fe25c0b0172849bb3b6f5b56148f18b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-28 16:15:21 -0800

    Add alternative solution

--- LeetCode/535. Encode and Decode TinyURL.js
+++ LeetCode/535. Encode and Decode TinyURL.js
@@ -35,3 +35,49 @@ var encode = function(longUrl) {
 var decode = function(shortUrl) {
     return links[shortUrl];
 };
+
+// This is somewhat similar to what I had considered.
+var map = {};
+var id = 10000;
+var chars = 'abcdefghijklmnopqrstuvwxyzZBCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.-'.split('');
+
+var base64 = function (number) {
+    let res = '';
+    while (number) {
+        res = chars[(number % 64)] + res;
+        number = Math.floor(number / 64);
+    }
+    return res;
+};
+
+var reBase64 = function (url) {
+    let res = 0;
+    url = url.split("").reverse().join("");
+    for (let i = 0; i < url.length; i++) {
+        res += (64 ** i) * chars.indexOf(url[i]);
+    }
+    return res;
+}
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    map[+id] = longUrl;
+    return base64(id);
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return map[reBase64(shortUrl)];
+};
+
+// 
