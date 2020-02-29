4e9a9ee94e8169d03d863708d46a78ccf3e7c888
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-02-28 16:25:18 -0800

    Add easy challenge

---
+++ LeetCode/58. Length of Last Word.js
@@ -0,0 +1,20 @@
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+// If the last word does not exist, return 0.
+
+// Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+// Example:
+
+// Input: "Hello World"
+// Output: 5
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+let lengthOfLastWord = function(s) {
+    const words = s.trim().split(" ");
+    return words[words.length - 1].length;
+};
