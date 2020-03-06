d1d539650ed73c5afe7b9971d77ddf61ca35c26f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-05 19:38:03 -0800

    Add alternative solution

--- LeetCode/66. Plus One.js
+++ LeetCode/66. Plus One.js
@@ -36,3 +36,18 @@ function plusOne(digits) {
     
     return digits;
 };
+
+// Simpler approach:
+var plusOne = function(digits) {
+    for(let i = digits.length - 1; i >= 0; i--) {
+      if(digits[i] === 9){
+        digits[i] = 0;
+      }
+      else {
+        digits[i]++;
+        return digits;
+      }
+    }
+    
+    return [1, ...digits];
+  };
