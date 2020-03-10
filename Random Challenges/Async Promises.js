e7234f580ae8d10de88dc0f99cca0b800b529d55
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-10 16:56:22 -0700

    Add simple test case

--- Random Challenges/Async Promises.js
+++ Random Challenges/Async Promises.js
@@ -2,14 +2,16 @@
 // All promises must be fired at the same time and all of them need to be executed before the function returns.
 
 function runAllPromises(inputList) {
-    for (let i = 0; i < inputList.legth; ++i){
-        inputList[i]()
+    return new Promise( ()=> {
+        for (let i = 0; i < inputList.legth; ++i){
+            inputList[i]()
             .fail(() => false);
-    }
+        }
+    });
 }
 
 // Ok, let's start considering that we do not care about them being fired concurrently. Let's fire them in order, as each promise resolves.
-function runAllPromises(inputList) {
+function runAllPromises2(inputList) {
     return new Promise((resolve, reject) => {
         if (!inputList.length) return resolve(true);
         else {
@@ -18,3 +20,18 @@ function runAllPromises(inputList) {
         }
     });
 }
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then(function(values) {
+  console.log(values);
+});
+runAllPromises([promise1, promise2, promise3])
+    .then(function(values) {
+    console.log(values);
+  });
+// expected output: 2x Array [3, 42, "foo"]
