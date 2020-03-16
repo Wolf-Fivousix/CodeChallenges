46d425cd10c8539d8a85c1d619de21fedfe4d99b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-16 13:51:00 -0700

    Add unsolved challenge

---
+++ Random Challenges/1.08 Price Change Script.js
@@ -0,0 +1,4 @@
+// FrontEnd Challange
+// ========================================
+// Javascript & jQuery
+// Write a script to auto-select and change the price of all apps to $100 under section  “Extra Integrations” on page https://www.odoo.com/pricing
