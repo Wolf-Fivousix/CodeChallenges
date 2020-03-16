c0769397ab31357480f78fbd4e40b100e76fd61a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-16 13:48:58 -0700

    Add unsolved challenge

---
+++ Random Challenges/1.05 Regex - Characters Preceded by Vowels.js
@@ -0,0 +1,3 @@
+// Backend Programming Challange
+// ========================================
+// Write a regex which can match all the occurrences of characters which are not immediately preceded by vowels.
