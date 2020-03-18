086dbc7ff62c3c7bda29a2d3384e79e3d69fff58
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-17 17:35:34 -0700

    Implement broken solution

--- Random Challenges/1.08 Price Change Script.js
+++ Random Challenges/1.08 Price Change Script.js
@@ -2,3 +2,6 @@
 // ========================================
 // Javascript & jQuery
 // Write a script to auto-select and change the price of all apps to $100 under section  “Extra Integrations” on page https://www.odoo.com/pricing
+
+document.getElementsByClassName("openerp_enterprise_pricing_step_body")[2].childNodes[1].childNodes
+// Will get the correct div, but there is no unique class identifier for all elements that needs to be changed.
