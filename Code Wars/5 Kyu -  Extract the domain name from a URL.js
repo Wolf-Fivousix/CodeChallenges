338d5713d53c480b798fe1686a97d5412844fb6a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-25 16:24:46 -0700

    Iterate solution to almost working state

--- Code Wars/5 Kyu -  Extract the domain name from a URL.js
+++ Code Wars/5 Kyu -  Extract the domain name from a URL.js
@@ -5,5 +5,8 @@
 // domainName("https://www.cnet.com") == "cnet"
 
 function domainName(url){
-    return url.match(/\/\/(www.)?\w+./g);
-  }
+    return url.match(/\/\/(www.)?.+?\./);
+}
+
+// This is currently grabbing the information desired, plus some extras, like the // and the following .
+// Now I need to find a way to keep them in the search, but remove them from the return value.
