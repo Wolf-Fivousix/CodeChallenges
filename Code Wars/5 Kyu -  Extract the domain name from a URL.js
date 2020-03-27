dac6067bba0aca4b825b578585923593f982297f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-03-26 17:42:52 -0700

    Add community solution and considerations

--- Code Wars/5 Kyu -  Extract the domain name from a URL.js
+++ Code Wars/5 Kyu -  Extract the domain name from a URL.js
@@ -12,3 +12,27 @@ function domainName(url){
     const slash = url.match(/\/\/(www.)?(.+?)\./);
     return slash ? slash[2] : url.match(/(www.)?(.+?)\./)[2];
 }
+
+// This solution is somewhat similar. It looks for the "https//www." and removes it, then just splits the result string on the "." and return the first element.
+function domainName(url){
+    return url.replace(/(https?:\/\/)?(www\.)?/, '').split('.')[0]
+}
+
+// Similar to the one above, but this ones removes everything except the domain name itself.
+function domainName(url){  
+    return url.replace(/.+\/\/|www.|\..+/g, '')
+}
+
+// This, THIS is the solution I was tryinig to come up with.
+// It finds all the variations for the start and then remove them from the resulting array.
+// [^\.]+ is going to match everything until it finds a dot, not including it.
+// But because we captured (www.) in another group, this ([^\.]) capture group will not have it inside. Effectively going bewond the first dot.
+
+function domainName(url){
+    return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+
+// We can remove the optional from the result to make a little more clear, if desired?
+function domainName(url){
+    return url.match(/(http(s)?:\/\/)?(w{3}\.)?([^\.]+)/i)[4];
+}
