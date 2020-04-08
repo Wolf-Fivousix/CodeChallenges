66b5f0149ca0896ce5682f069370d78aa372576d
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-07 18:22:19 -0700

    Add challenge with pseudocode

---
+++ Random Challenges/2.05 Triplet Algorithmic Progression.js
@@ -0,0 +1,8 @@
+// Given an unsorted array of positive integers, find the triplets that behave as a positive Aritimethic Progression.
+//  AP example: 1, 3, 5 => 2    5, 10, 15 => 5
+
+// sort array
+// iterate I
+//     iterate J
+//         iterate K
+//             if K - J === J - I      I + K === J * 2
