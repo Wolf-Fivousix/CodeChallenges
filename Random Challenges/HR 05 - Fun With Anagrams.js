757c2929f3bfc51136235454387ee314e714178e
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-16 16:49:25 -0700

    Add solved challenge

--- Random Challenges/HR 05 - Fun With Anagrams.js
+++ Random Challenges/HR 05 - Fun With Anagrams.js
@@ -1,7 +1,46 @@
+// Two strings are anagrams if they
+// are permutations of each other. In
+// other words, both strings have the
+// same size and the same
+// characters. For example,
+// "aaagmnrs" is an anagram of
+// "anagrams". Given an array of
+// strings, remove each string that is
+// an anagram of an earlier string,
+// then return the remaining array in
+// sorted order.
+// Example
+// str = ['code', 'doce', 'ecod', 'framer',
+// 'frame']
+// "code" and "doce" are anagrams.
+// Remove "doce" from the array
+// and keep the first occurrence
+// "code" in the array.
+// "code" and "ecod" are anagrams.
+// Remove "ecod" from the
+// array and keep the first
+// occurrence "code" in the array.
+// "code" and "framer" are not
+// anagrams. Keep both strings in
+// the array.
+// "framer" and "frame" are not
+// anagrams due to the extra 'r' in
+// 'framer'. Keep both strings in the
+// array.
+// Order the remaining strings in
+// ascending order: [
+// "code","frame","framer"].
+// Function Description
+// Complete the function
+// funWithAnagrams in the editor
+// below.
+// funWithAnagrams has the
+// following parameters:
+
 // Fun With Anagrams
 
 // Two strings are anagrams if they are permutations of each other.
-// In other words, both stirngs have the same size and the same characters.
+// In other words, both strings have the same size and the same characters.
 // For example, "aaagmnrs" is an anagram of "anagrams". Given an array of strings, remove
 // each stirng that is an anagram of an earlier string, then retur nthe remaining array in 
 // sorted order.
@@ -21,7 +60,6 @@ function funWithAnagrams(text) {
     let words = [];
     for (let i = 0; i < text.length; ++i) {
         if (neverAnagram(words, text[i])) {
-            // console.log("pushing", text[i]);
             words.push(text[i]);
         }
     }
@@ -31,7 +69,7 @@ function funWithAnagrams(text) {
 
 function neverAnagram(array, word) {
     for (let i = 0; i < array.length; ++i) {
-        if (isANagram(array[i], word)) {
+        if (isAnagram(array[i], word)) {
             return false;
         }
     }
