f5af622b33df9c1735a61ecf89740b75fb3acbb6
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-16 19:45:48 -0700

    Refactor solution

--- LeetCode/83. Remove Duplicates from Sorted List.js
+++ LeetCode/83. Remove Duplicates from Sorted List.js
@@ -52,3 +52,31 @@ function notFound(head, val) {
 
 // This solution is not very efficient. It is a brute force approach.
 // If the list is made of dinstinct items, this will take Polynomial complexity. O(n^2)
+
+function deleteDuplicates(head) {
+    if (!head) return null;
+    
+    let newList = new ListNode(head.val);
+    let listHead = newList;
+    let tail = head.next;
+    let hash = {};
+    hash[listHead.val] = true;
+    
+    while (tail) {
+        if (!hash[tail.val]) {
+            hash[tail.val] = true;
+            let node = new ListNode(tail.val);
+            newList.next = node;
+            newList = newList.next;
+        }
+        tail = tail.next;
+    }
+    
+    return listHead;
+};
+
+// Now this approach will use more memory, as we will have a Hash that holds every single value.
+// On top of the aditional linkedList. But with that we only need to iterate through the list once.
+// Making it a Linear Time Complexity. O(n).
+// I think we can still do better. What if we can just delete the elements from the original list?
+
