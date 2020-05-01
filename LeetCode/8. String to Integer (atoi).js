f6fffc74e216eb50bef24e32e0239414a6ab38ff
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-04-30 17:14:47 -0700

    Solve challenge

--- LeetCode/8. String to Integer (atoi).js
+++ LeetCode/8. String to Integer (atoi).js
@@ -44,14 +44,65 @@
  * @param {string} str
  * @return {number}
  */
-function myAtoi(str) {
-    console.log(str.match(/[+-]?\d+/));
+function myAtoi(str) {   
+    const chars = {
+        " ": true,
+        "+": true,
+        "-": true,
+        "0": true,
+        "1": true,
+        "2": true,
+        "3": true,
+        "4": true,
+        "5": true,
+        "6": true,
+        "7": true,
+        "8": true,
+        "9": true
+    };
+    
+    const digits = {
+        "0": true,
+        "1": true,
+        "2": true,
+        "3": true,
+        "4": true,
+        "5": true,
+        "6": true,
+        "7": true,
+        "8": true,
+        "9": true
+    }
+    
+    for (let i = 0; i < str.length; ++i) {
+        const char = str[i];
+        if (!chars[char]) return 0;
+        else if (char !== " ") {
+            if (char === "+" || char === "-") {
+               const followUp = str[i+1];
+               if (!digits[followUp]) return 0; 
+            }
+            else break;
+        }
+    }
+    
+    let number = str.match(/(\s+)?[+-]?\d+/);
+    number = number ? number[0] : 0;
+    // console.log(Number(number));
+    
+    if (-(2**31) < number && number < (2**31 - 1)) return number;
+    return number < 0 ? -(2**31) : (2**31 -1);
     
     
 };
 
+// This is a working solution, but it does not pass the specs of Hacker Rank due to "+-2" having to be considered an invalid solution....
+// The cases "+-2", "-   1345" forced me to have a separate hash for the digits.
+
 myAtoi("42");
 myAtoi("   -42");
+myAtoi("  +word 1234");
 myAtoi("+4193 with words");
 myAtoi("words and 987");
 myAtoi("-91283472332");
+myAtoi("-912834723321654651654168");
