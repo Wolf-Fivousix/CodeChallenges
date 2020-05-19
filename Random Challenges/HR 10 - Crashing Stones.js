91a6c2dbaf103eb583d10abbebd0fedee604c824
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-18 20:20:01 -0700

    Add 9/11 solution by Stanley Traub

--- Random Challenges/HR 10 - Crashing Stones.js
+++ Random Challenges/HR 10 - Crashing Stones.js
@@ -80,3 +80,17 @@ function naiveInsert(array, value) {
     return;
 }
 // 7/11 test cases - Need to make the sorting efficiency issue.
+
+
+// 9/11 Test Cases by Stanley Traub.
+var lastStoneWeight = function (stones) {
+    if (stones.length < 2) return stones;
+    while (stones.length > 1) {
+        let index1 = stones.indexOf(Math.max(...stones));
+        let stone1 = stones.splice(index1, 1);
+        let index2 = stones.indexOf(Math.max(...stones));
+        let stone2 = stones.splice(index2, 1);
+        stones.push(stone1 - stone2);
+    }
+    return stones;
+};
