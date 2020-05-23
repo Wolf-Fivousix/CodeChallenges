b14ef219f361a523926aa63298e4f084cef5ed68
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-22 19:22:27 -0700

    Add a peer solution

--- Random Challenges/HR 09 - Construction Management.js
+++ Random Challenges/HR 09 - Construction Management.js
@@ -144,6 +144,31 @@ function minCost(cost) {
 // The logic here works.... Now, how can I change this to NOT use this much memory?
 
 
+// Solution by Sarah
+function minCost(cost) {
+  let costs = cost[0];
+  let currCosts = new Array(3).fill(0);
+  for (let i = 1; i < cost.length; i++) {   // loop through each of the houses
+    let houseCosts = cost[i];
+    for (let matIdx = 0; matIdx < houseCosts.length; matIdx++) {
+      switch (matIdx) {
+        case 0:
+          currCosts[matIdx] = houseCosts[matIdx] + Math.min(costs[1], costs[2])
+          break;
+        case 1:
+          currCosts[matIdx] = houseCosts[matIdx] + Math.min(costs[0], costs[2])
+          break;
+        case 2:
+          currCosts[matIdx] = houseCosts[matIdx] + Math.min(costs[0], costs[1])
+          break;
+      }
+    }
+    costs = currCosts;
+    currCosts = [];
+  }
+  return Math.min(costs[0], costs[1], costs[2]);
+}
+
 const test1 = [ [ 1, 2, 2 ], [ 2, 2, 1 ], [ 2, 1, 2 ] ];
 // => 3
 const test2 = [ [ 1, 2, 2 ], [ 2, 3, 3 ], [ 3, 3, 1 ] ];
