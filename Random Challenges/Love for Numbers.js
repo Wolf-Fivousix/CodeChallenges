a4424a671dc4b953c6b3e9b3d0bce35b195dce0e
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-26 11:53:19 -0700

    Refactor solution

--- Random Challenges/Love for Numbers.js
+++ Random Challenges/Love for Numbers.js
@@ -1,39 +1,40 @@
 // Sally loves numbers that have unique digits. Given a range find how many numbers sally loves.
 
 function countNumbers(arr) {
+    const lovedNumbers = {};
+
     arr.forEach(rangeArray => {
         let counter = 0;
+
         for (let number = rangeArray[0]; number <= rangeArray[1]; ++number) {
-            if (love(number)) ++counter;
+            if (lovedNumbers[number] || love(number, lovedNumbers)) ++counter;
         }
 
         console.log(counter);
     });
 }
 
-// This solution works fine, but for big enough ranges, the heap will explode due lack of memory.
-function countNumbers(arr) {
-    let ranges = [];
-    arr.forEach(subArray => ranges.push(buildRange(subArray)));
-    
-    ranges.forEach(range => console.log(range.filter(number => love(number)).length));
-}
-
-function buildRange(array) {
-    let range = [];
-    for (let i = array[0]; i <= array[1]; ++i) {
-        range.push(i);
-    }
-    
-    return range;
-}
-
-function love(number) {
+function love(number, lovedNumbers) {
+    const original = number;
     let digits = {};
-    const values = String(number).split("");
-    for(let i = 0; i < values.length; ++i) {
-        if (digits[values[i]]) return false;
-        else digits[values[i]] = true;
+
+    while(number > 0) {
+        const digit = number % 10;
+
+        if (digits[digit]) {
+            lovedNumbers[original] = false;
+            return false;
+        }
+        else digits[digit] = true;
+
+        number = Math.floor(number/10);
     }
+
+    lovedNumbers[original] = true;
     return true;
 }
+
+// By using the Hash for previously calculated numbers we can improve the run time by a bit.
+// Passing 9/11 Test Cases.
+
+// Even though we burn memory, we cannot execute quick enough for 5k and 10k ranges.
