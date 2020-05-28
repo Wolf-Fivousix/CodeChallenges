ade386d02e92318b83e3cb4b48e1ec885711f18c
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-05-27 21:20:02 -0700

    Add solution by Max

--- Random Challenges/W1D1-A Self Dividing Numbers.js
+++ Random Challenges/W1D1-A Self Dividing Numbers.js
@@ -59,3 +59,41 @@ function selfDividing(value) {
 console.log(findSelfDividingNumbers(44, 46));
 console.log(findSelfDividingNumbers(7, 4));
 // console.log(findSelfDividingNumbers(1556, 35));
+
+
+
+//////////////////////////////////////////////////
+// Solution by Max
+//////////////////////////////////////////////////
+// A self-dividing number is a number that is divisible by every digit it contains. 
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0,and 128 % 8 == 0.
+
+// Also, a self-dividing number is not allowed to contain the digit zero.
+
+// Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+// Example
+// Input: left = 1, right = 22
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+// (128, 129)
+
+const sdf = function(left, right) {
+    let result = [];
+    for (let i = left; i <= right; i++) {
+        let num = String(i); // '1'
+        let flag = true;
+        for (let j = 0; j < num.length; j++) {
+            if (i % Number(num[j]) !== 0) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+
+// console.log(sdf(1, 22));
