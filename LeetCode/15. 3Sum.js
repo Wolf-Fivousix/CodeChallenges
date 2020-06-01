5228613193249248f3dca605a9bf7fea7f7b5731
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-01 16:43:42 -0700

    Add optimization to polynomial solution

--- LeetCode/15. 3Sum.js
+++ LeetCode/15. 3Sum.js
@@ -61,3 +61,29 @@ function hasValues(array, values) {
 
 // This solution works, but since it is a brute force approach (Polynomial O^3), it does not run within the time limit.
 // How can we make it better?
+
+function threeSum(nums) {
+    const results = [];
+    nums = nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length - 2; ++i) {
+        if (nums[i] > 0) return results;
+        
+        for (let j = i + 1; j < nums.length - 1; ++j) {
+            const sum = nums[i] + nums[j];
+            // if (nums[i] === -6 && nums[j] === 3) console.log(sum);
+            if (nums[j] > 0 && sum > 0) continue;
+            
+            for (let k = j + 1; k < nums.length; ++k) {
+                if (nums[i] + nums[j] + nums[k] === 0) addUniqueArray(results, [nums[i], nums[j], nums[k]]);
+            }
+        }
+    }
+    
+    return results;
+};
+
+// Here we have a small optimization of the Polynomial loop. By sorting the input we can now guarantee that once the sum is positive
+// and the current number is also positive, no number following it will make the sum reach 0.
+// It gave us a little bit of room, but not enough.
+// Can we make it better?
