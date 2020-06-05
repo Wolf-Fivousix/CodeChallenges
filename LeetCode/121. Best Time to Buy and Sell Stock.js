aac5d2918fe6043f6e3ede54f7a60ae78b064bf9
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-04 19:49:26 -0700

    Refactor solution and add notes on both solutions

--- LeetCode/121. Best Time to Buy and Sell Stock.js
+++ LeetCode/121. Best Time to Buy and Sell Stock.js
@@ -41,6 +41,34 @@ function maxProfit(prices) {
 // Runtime: 80 ms, faster than 36.17% of JavaScript online submissions for Best Time to Buy and Sell Stock.
 // Memory Usage: 37.1 MB, less than 7.41% of JavaScript online submissions for Best Time to Buy and Sell Stock.
 
+// I ran this code again for a couple times, this is what I got:
+// Runtime: 64 ms, faster than 60.46% of JavaScript online submissions for Best Time to Buy and Sell Stock.
+// Memory Usage: 37 MB, less than 17.25% of JavaScript online submissions for Best Time to Buy and Sell Stock.
+
+
 // We're taking a very simple approach here. We move once through our input looking for the lowest price so far, and save it.
 // Then we work backwards looking for the best spread (the difference between buy and sell prices).
 // This is a Linear Time and Space Complexity.
+
+
+// This is the proposed solution. Linear time and Constant Space.
+function maxProfit(prices) {
+    let minPrice = Number.POSITIVE_INFINITY;
+    let maxProfit = 0;
+    
+    for (let i = 0; i < prices.length; ++i) {
+        if (prices[i] < minPrice) minPrice = prices[i];
+        else if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice;
+    }
+    
+    return maxProfit;
+};
+
+// Runtime: 68 ms, faster than 50.64% of JavaScript online submissions for Best Time to Buy and Sell Stock.
+// Memory Usage: 37 MB, less than 17.82% of JavaScript online submissions for Best Time to Buy and Sell Stock.
+
+// As we can see, the run time is a little better, but negligible since the order of magnitude is the same.
+// The memory size though, is marginally better. Given, for a large enough input the differences will grow.
+
+// After comparing both runtimes, we can conclude that for changes on the same scale of magnitude, Leet Code run/space numbers are
+// not reliable.
