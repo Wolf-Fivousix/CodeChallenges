dff6d91e122a5d75fc7df88faf6081c667eda5a1
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-09 16:40:08 -0700

    Add proposed solutions

--- LeetCode/0101. Symmetric Tree.js
+++ LeetCode/0101. Symmetric Tree.js
@@ -86,3 +86,36 @@ function getRightBranch(root) {
 // For the left we read nodes left to right.
 // For the right we read nodes right to left.
 // The two functions could even be merged into one by using a flag in which direction we want to move.
+
+// These are the proposed solutions (in Java):
+// Recursive
+// public boolean isSymmetric(TreeNode root) {
+//     return isMirror(root, root);
+// }
+
+// public boolean isMirror(TreeNode t1, TreeNode t2) {
+//     if (t1 == null && t2 == null) return true;
+//     if (t1 == null || t2 == null) return false;
+//     return (t1.val == t2.val)
+//         && isMirror(t1.right, t2.left)
+//         && isMirror(t1.left, t2.right);
+// }
+
+// Iterative
+// public boolean isSymmetric(TreeNode root) {
+//     Queue<TreeNode> q = new LinkedList<>();
+//     q.add(root);
+//     q.add(root);
+//     while (!q.isEmpty()) {
+//         TreeNode t1 = q.poll();
+//         TreeNode t2 = q.poll();
+//         if (t1 == null && t2 == null) continue;
+//         if (t1 == null || t2 == null) return false;
+//         if (t1.val != t2.val) return false;
+//         q.add(t1.left);
+//         q.add(t2.right);
+//         q.add(t1.right);
+//         q.add(t2.left);
+//     }
+//     return true;
+// }
