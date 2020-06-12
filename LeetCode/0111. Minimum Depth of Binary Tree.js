dc93fe538ef365b7d6a5221d959cab91384c285f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-12 16:44:13 -0700

    Add solution to Easy problem

--- LeetCode/0111. Minimum Depth of Binary Tree.js
+++ LeetCode/0111. Minimum Depth of Binary Tree.js
@@ -47,3 +47,18 @@ function minDepth(root) {
 
 // Something here makes no sense. Supposedly the Depth is the number of nodes between the root and the closest leaf node.
 // For some reason, though, the test cases are not passing.
+// It doesn't pass because a node is only considered a leaf if it has no children.
+
+function minDepth(root, depth = 0) {
+    if (!root) return depth;
+    ++depth;
+    
+    if (!root.left && !root.right) return depth;
+    if (root.left && root.right) return Math.min(minDepth(root.left, depth), minDepth(root.right, depth));
+    
+    const node = root.left || root.right;
+    return minDepth(node, depth);
+};
+
+// Runtime: 80 ms, faster than 31.60% of JavaScript online submissions for Minimum Depth of Binary Tree.
+// Memory Usage: 40 MB, less than 6.24% of JavaScript online submissions for Minimum Depth of Binary Tree.
