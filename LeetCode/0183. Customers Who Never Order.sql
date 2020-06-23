21c13d856892b2351f1e35eeb36a390478a888e7
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-23 13:17:37 -0700

    Add solved Easy problem

---
+++ LeetCode/0183. Customers Who Never Order.sql
@@ -0,0 +1,39 @@
+-- Easy
+
+-- Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+-- Table: Customers.
+
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Table: Orders.
+
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Using the above tables as example, return the following:
+
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+SELECT customers.name AS Customers
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customerId
+WHERE orders.customerId IS NULL
+;
+
+-- Runtime: 460 ms, faster than 88.52% of MySQL online submissions for Customers Who Never Order.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Customers Who Never Order.
