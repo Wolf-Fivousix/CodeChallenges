e8f04ee2a0f262d082c00680da87da8ec9b03b3b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-23 12:58:15 -0700

    Add community solutions

--- LeetCode/0168. Excel Sheet Column Title.js
+++ LeetCode/0168. Excel Sheet Column Title.js
@@ -45,3 +45,31 @@ function convertToTitle(n) {
 
 // Runtime: 68 ms, faster than 31.22% of JavaScript online submissions for Excel Sheet Column Title.
 // Memory Usage: 33.1 MB, less than 78.52% of JavaScript online submissions for Excel Sheet Column Title.
+
+// Solution by Valaamm
+// var convertToTitle = function(n) {
+//     let result = '';
+//     while (n > 0) {
+//         result = String.fromCharCode(64 + ( (n % 26) || 26) ) + result;
+//         n = Math.ceil( n / 26) - 1;
+//     };
+//     return result;
+// };
+
+// Solution by achandel
+// var convertToTitle = function(n) {
+//     let title = '';
+//     // loop until n > 0
+//     while(n > 0) {
+//         // decrement n by 1 (to deal with number divisible by 26)
+// 			n--;
+//         // mod n by 26 and append corresponding char (one previous char) into result string
+// 			title += String.fromCharCode(65 + (n % 26));
+//         // divide n by 26 and reset that value as n
+// 			n = parseInt(n / 26);
+//     }
+//     // return reversed result string (i.e. title)
+// 		return title.split('').reverse().join('');
+    
+// };
+
