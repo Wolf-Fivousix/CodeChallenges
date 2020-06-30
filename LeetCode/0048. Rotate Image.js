c8dc2d521624d9af5ed4c4cb25462e67a5e810b1
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-29 17:43:44 -0700

    Add community solutions

--- LeetCode/0048. Rotate Image.js
+++ LeetCode/0048. Rotate Image.js
@@ -62,3 +62,39 @@ function rotate(matrix) {
 };
 
 // This is not the correct answer, but it manages to flip the matrix.
+
+
+// Community solutions:
+
+// By AminiCK
+// The Idea
+// Transpose the matrix
+// Reverse each row
+var rotate = function(matrix) {
+    for (let i=0;i<matrix.length;i++) {
+        for (let j=i;j<matrix[0].length;j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (let i=0;i<matrix.length;i++) {
+        for (let j=0;j<matrix[0].length/2;j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrix[0].length-j-1];
+            matrix[i][matrix[0].length-j-1] = temp;
+        }
+    }
+};
+
+// By haleyysz
+
+var rotate = function(matrix) {
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = i+1; j < matrix[0].length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+        matrix[i] = matrix[i].reverse()
+    }
+};
