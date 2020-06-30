bc1af4b3c029242e87096041284cc753dc10462a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-06-29 17:31:46 -0700

    Add unsolved Medium problem

---
+++ LeetCode/0048. Rotate Image.js
@@ -0,0 +1,64 @@
+// Medium
+
+// You are given an n x n 2D matrix representing an image.
+
+// Rotate the image by 90 degrees (clockwise).
+
+// Note:
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// Example 1:
+
+// Given input matrix = 
+// [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9]
+// ],
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [7,4,1],
+//   [8,5,2],
+//   [9,6,3]
+// ]
+// Example 2:
+
+// Given input matrix =
+// [
+//   [ 5, 1, 9,11],
+//   [ 2, 4, 8,10],
+//   [13, 3, 6, 7],
+//   [15,14,12,16]
+// ], 
+
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [15,13, 2, 5],
+//   [14, 3, 4, 1],
+//   [12, 6, 8, 9],
+//   [16, 7,10,11]
+// ]
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix) {
+    const newMatrix = [];
+    for (let i = 0; i < matrix.length; ++i) {
+        newMatrix.push(new Array(matrix.length).fill(0));
+    }
+    
+    for (let row = 0; row < matrix.length; ++row) {
+        for (let column = 0; column < matrix.length; ++column) {
+            newMatrix[column][matrix.length - 1 - row] = matrix[row][column];
+        }
+    }
+    
+    // console.log(newMatrix);
+    return newMatrix;
+};
+
+// This is not the correct answer, but it manages to flip the matrix.
