af2c11641543d5910b311d1b9738b4dd672519f4
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-08 22:50:04 -0700

    Add solution for Medium problem

--- LeetCode/0064. Minimum Path Sum.js
+++ LeetCode/0064. Minimum Path Sum.js
@@ -32,3 +32,39 @@ function minPathSum(grid, row = 0, column = 0) {
 
 // Brute Force approach. Does not compute within the time limits.
 // How could we use Memoization to avoid duplicate work?
+
+function minPathSum(grid) {
+	const resultGrid = [];
+    
+    for (let row = 0; row < grid.length; ++row) {
+        const currentRow = [];
+        
+        for (let column = 0; column < grid[row].length; ++column) {
+            let up = row - 1 < 0 ? Number.POSITIVE_INFINITY : resultGrid[row - 1][column];
+            const left = column - 1 < 0 ? Number.POSITIVE_INFINITY : currentRow[column - 1];
+            
+            if (row - 1 < 0 && column - 1 < 0) up = 0;
+            currentRow.push(grid[row][column] + Math.min(up, left));
+        }
+
+    resultGrid.push(currentRow);
+    }
+
+    return resultGrid[resultGrid.length - 1][resultGrid[resultGrid.length - 1].length - 1];
+}
+
+// Ok. Now this is much better.
+// The catch here is calculating the best optimal path up to the point we are.
+// Because the "movement" only happens to the left and down, that is the same pattern
+// that we traverse the grid as well. That means that when we reach a point X, Y, we have
+// already visited the point X - 1 and Y - 1, allowing us to look those two values and say:
+// Which of this paths is better? Once we have a more optimal path, there is no reason for us to
+// store the least optimal, so we just throw that computation away and move on.
+// When we reach the very last element, we have succesfully calculated the optimal path for both
+// positions that leads to the end. Now we just need to pick which one is the best path.
+
+// That makes for Linear Time and Space Complexity.
+// If we modify the original grid, we can do with Constant Space.
+
+// Runtime: 76 ms, faster than 56.09% of JavaScript online submissions for Minimum Path Sum.
+// Memory Usage: 38.3 MB, less than 32.03% of JavaScript online submissions for Minimum Path Sum.
