8aaee968401feab46d209538f14dca4a64529293
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-14 11:22:17 -0700

    Add solution to Easy problem

--- LeetCode/0198. House Robber.js
+++ LeetCode/0198. House Robber.js
@@ -30,14 +30,27 @@
  * @return {number}
  */
 function rob(nums) {
-    let one = 0;
-    let two = 0;
+    let currentMax = 0;
+    let previousMax = 0;
+    
     for (let i = 0; i < nums.length; ++i) {
-        if (i % 2) two += nums[i];
-        else one += nums[i];
+        const temp = currentMax;
+        currentMax = Math.max(currentMax, previousMax + nums[i]);
+        previousMax = temp;
     }
     
-    return Math.max(one, two);
+    return Math.max(currentMax, previousMax);
 };
 
-// What about when we want to skip a point? Like [2, 1, 1, 2] best result is 4, not 3.
+// Runtime: 64 ms, faster than 74.81% of JavaScript online submissions for House Robber.
+// Memory Usage: 32.8 MB, less than 94.58% of JavaScript online submissions for House Robber.
+
+// Linear Time Complexity and Constant Space Complexity.
+
+// Here's the idea:
+// We keep track of two variables: One that choose to rob the current house.
+// One that did not rob the current house.
+// When we move to the next house, we compare and say: "If I rob this house, would I be better of not robing the previous house?"
+// If the answer is "yes", then we "unrob" the previous house and rob the current house. (Yeah, I know, magic)
+// They key point is: We need to keep a record of these two houses in order to "not rob" the previous one IN CASE the current house
+// would make us better of. But if the previous house loot was good enough, swapping between it and the current is not worth it.
