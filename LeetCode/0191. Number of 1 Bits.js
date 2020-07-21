ad836f1179643d6af3c5d64a72b3b60bdce3c305
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-20 17:41:22 -0700

    Add community solutions

--- LeetCode/0191. Number of 1 Bits.js
+++ LeetCode/0191. Number of 1 Bits.js
@@ -45,3 +45,35 @@ function hammingWeight(n) {
 
 // Constant Time and Space complexity. Because the input is always a 32bit integer, the ammount of work
 // we do and space used never changes.
+
+// Solution by mmartire.
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let sum = 0;
+    
+    while(n != 0) {
+        sum += n & 1;
+        n = n >>> 1;
+    }
+    
+    return sum;
+};
+
+// I'm adding this solution for the usage of AND.
+// The work is similar, without the conversion into a binary string.
+// He AND's the number to 000000001. Which means, if the first digit of N is 1, we'll get 1.
+// Otherwise, it will be 0. So it just sums that up to a counter.
+// And then it uses the "unsigned right sfhit" >>> to reduce the number. (same as dividing by 2).
+
+// From W3 School
+// >>	Signed right shift	Shifts right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+// >>>	Zero fill right shift	Shifts right by pushing zeros in from the left, and let the rightmost bits fall off
+
+// Solution by valaamm
+// This is a smart trick that I found intersting.
+var hammingWeight = function(n) {
+    return n.toString(2).split('1').length - 1;
+};
