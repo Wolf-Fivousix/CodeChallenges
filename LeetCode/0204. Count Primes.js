28d13dc22c017fce511c735a8da0d4e47d3bfe1e
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-20 21:07:47 -0700

    Add community solutions

--- LeetCode/0204. Count Primes.js
+++ LeetCode/0204. Count Primes.js
@@ -59,3 +59,46 @@ function countPrimes(n) {
 }
 
 // New version. Still running out of time. =/
+
+// This theory: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+// Solution by AminiCK
+// The Idea
+// Use Sieve of Eratosthenes Algorithm
+// Mark all increment of current value to false. p*(p+1), p*(p+2) ...
+// The total Iteration run time can reduce to sqrt(n)
+var countPrimes = function(n) {
+    let hash = new Array(n).fill(true);
+    hash[0] = false;
+    hash[1] = false;
+    for (let i=2;i*i<n;i++) {
+        if (hash[i]) {
+            for(let j=i*i;j<n;j+=i){ // p*(p+1)...
+                hash[j] = false;
+            }
+        }
+    }
+    return hash.filter((val)=>val).length;
+};
+// Runtime: 140 ms, faster than 80.69% of JavaScript online submissions for Count Primes.
+// Memory Usage: 65 MB, less than 35.39% of JavaScript online submissions for Count Primes.
+
+// Solution by sushman
+var countPrimes = function(n) {
+    var count = 0;
+    var flag = true;
+    for(var i=2; i<n; i++){
+        flag=true;
+        for(var j = 2; j <= Math.sqrt(i); j++){
+            if(i%j==0){
+                flag = false;
+                break;    
+            }
+        }
+        if(flag==true)
+			count=count+1;
+    }
+    return count;
+};
+// Runtime: 880 ms, faster than 15.79% of JavaScript online submissions for Count Primes.
+// Memory Usage: 36.6 MB, less than 73.66% of JavaScript online submissions for Count Primes.
