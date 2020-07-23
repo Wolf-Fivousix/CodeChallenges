ce85cc5ce7c050f6ba35be19e69cf10f01fede7d
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-23 15:12:14 -0700

    Add community solution

--- LeetCode/0234. Palindrome Linked List.js
+++ LeetCode/0234. Palindrome Linked List.js
@@ -52,3 +52,21 @@ function isPalindrome(head) {
 
 // For the follow up, though, how can we still do this in linear time and constant memory?
 // I can think of doing the traversal Polynomial and checking from end to start of the list...
+
+// Solution by DawChihLiou
+var isPalindrome = function(head) {
+    let curr = head;
+    const traverse = node => {
+        if (node === null) {
+            return true;
+        }
+        // traverse to the end of the list first
+        const prevIsSame = traverse(node.next);
+        // when the call stack bounces back, compare the values 
+        // from the head and from the bottom up
+        const currIsSame = curr.val === node.val;
+        curr = curr.next;
+        return prevIsSame && currIsSame;
+    }
+    return traverse(head);
+};
