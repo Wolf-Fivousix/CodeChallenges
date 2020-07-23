e3f70370aad5351630e28787f3f38d429cbf8851
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-23 14:56:01 -0700

    Add solution to Easy problem

--- LeetCode/0234. Palindrome Linked List.js
+++ LeetCode/0234. Palindrome Linked List.js
@@ -13,3 +13,42 @@
 // Follow up:
 // Could you do it in O(n) time and O(1) space?
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function isPalindrome(head) {
+    let sequence = [];
+    
+    while (head) {
+        sequence.push(head.val);
+        head = head.next;
+    }
+    
+    let left = 0;
+    let right = sequence.length - 1;
+    while (left < right) {
+        if (sequence[left] !== sequence[right]) return false;
+        ++left;
+        --right;
+    }
+    
+    return true;
+}
+
+// Runtime: 76 ms, faster than 76.43% of JavaScript online submissions for Palindrome Linked List.
+// Memory Usage: 41.4 MB, less than 20.00% of JavaScript online submissions for Palindrome Linked List.
+
+// For this I decided to convert the Single Liked List into an array.
+// By using memory we can circunvent the issues with traversing the list multiple times.
+// This makes for Linear Time and Space complexity.
+
+// For the follow up, though, how can we still do this in linear time and constant memory?
+// I can think of doing the traversal Polynomial and checking from end to start of the list...
