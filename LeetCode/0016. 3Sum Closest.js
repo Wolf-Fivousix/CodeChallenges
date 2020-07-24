d6cab05dcd4da3b0811de88e61604b13ccc4ce70
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-24 15:38:58 -0700

    Add proposed solution

--- LeetCode/0016. 3Sum Closest.js
+++ LeetCode/0016. 3Sum Closest.js
@@ -42,3 +42,38 @@ function threeSumClosest(nums, target) {
 
 // Runtime: 200 ms, faster than 12.98% of JavaScript online submissions for 3Sum Closest.
 // Memory Usage: 37.3 MB, less than 23.17% of JavaScript online submissions for 3Sum Closest.
+
+// Proposed solution N ^ 2
+// Algorithm
+
+// Initialize the minimum difference diff with a large value.
+// Sort the input array nums.
+// Iterate through the array:
+// For the current position i, set lo to i + 1, and hi to the last index.
+// While the lo pointer is smaller than hi:
+// Set sum to nums[i] + nums[lo] + nums[hi].
+// If the absolute difference between sum and target is smaller than the absolute value of diff:
+// Set diff to target - sum.
+// If sum is less than target, increment lo.
+// Else, decrement hi.
+// If diff is zero, break from the loop.
+// Return the value of the closest triplet, which is target - diff.
+
+// C++
+// int threeSumClosest(vector<int>& nums, int target) {
+//     int diff = INT_MAX, sz = nums.size();
+//     sort(begin(nums), end(nums));
+//     for (int i = 0; i < sz && diff != 0; ++i) {
+//         int lo = i + 1, hi = sz - 1;
+//         while (lo < hi) {
+//             int sum = nums[i] + nums[lo] + nums[hi];
+//             if (abs(target - sum) < abs(diff))
+//                 diff = target - sum;
+//             if (sum < target)
+//                 ++lo;
+//             else
+//                 --hi;
+//         }
+//     }
+//     return target - diff;
+// }
