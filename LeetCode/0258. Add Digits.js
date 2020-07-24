12704384dacb6d72cdfb00591896c1981f95de3f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-24 15:29:49 -0700

    Add proposed solution

--- LeetCode/0258. Add Digits.js
+++ LeetCode/0258. Add Digits.js
@@ -35,3 +35,23 @@ function sumDigits(number) {
 // Memory Usage: 38.1 MB, less than 5.75% of JavaScript online submissions for Add Digits.
 // "Linear" Time Complexity, although the run time does not quite grow with the input, like 111111 and 999.
 // Constant Space Complexity.
+
+// The Constant solution is, as always, a math trick:
+// "The original number is divisible by 9 if and only if the sum of its digits is divisible by 9".
+// In other words, the number will be eiter a multiple of 9 or the remaining value to be a multiple of 9.
+
+// class Solution {
+//     public int addDigits(int num) {
+//         if (num == 0) return 0;
+//         if (num % 9 == 0) return 9;
+//         return num % 9;
+//     }
+// }
+
+// or
+
+// class Solution {
+//     public int addDigits(int num) {
+//         return num == 0 ? 0 : 1 + (num - 1) % 9;
+//     }
+// }
