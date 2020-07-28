8ff1f02579b5a73de99c8a7d27d2eb3a0b21071f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-28 10:35:13 -0700

    Add community solution

--- LeetCode/0039. Combination Sum.js
+++ LeetCode/0039. Combination Sum.js
@@ -100,3 +100,22 @@ function combinationSum(candidates, target) {
 
 // We are doing a Polynomial Time Complexity. Trying every single possible combination.
 // We are also making multiple copies of arrays in order to calculate all that, so Polynomial Space Complexity as well.
+
+// Solution by lifongi
+function combinationSum2(candidates, target) {
+    candidates.sort((a, b) => a - b);
+    var buffer = [];
+    var result = [];
+    search(0, target);
+    return result;
+  
+    function search(startIdx, target) {
+      if (target === 0) return result.push(buffer.slice());
+      if (target < 0) return;
+      if (startIdx === candidates.length) return;
+      buffer.push(candidates[startIdx]);
+      search(startIdx, target - candidates[startIdx]);
+      buffer.pop();
+      search(startIdx + 1, target);
+    }
+  }
