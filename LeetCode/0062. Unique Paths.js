2c25b458ab70ff77c41d33331f0778f0bbc428b4
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-07-30 18:01:50 -0700

    Add solution to Medium problem

--- LeetCode/0062. Unique Paths.js
+++ LeetCode/0062. Unique Paths.js
@@ -47,5 +47,42 @@ function uniquePaths(m, n) {
     // From those points, the possibility space grows as
     // the sum from the possibilities BELLOW and TO THE RIGHT.
     // So for input 2x2, the possibilities are 2.
-
+    if (m === 1 || n === 1) return 1;
+    const grid = makeGrid(m, n);
+    
+    // I'm flipping the finish and start during iteration.
+    for (let row = 0; row < m; ++row) {
+        for (let column = 0; column < n; ++column) {
+            if (!row && !column) {
+                grid[0][0] = 1;
+                continue;
+            }
+            
+            let sum = 0;
+            sum += row - 1 < 0 ? 0 : grid[row - 1][column];
+            sum += column - 1 < 0 ? 0 : grid[row][column - 1];
+            grid[row][column] = sum;
+        }
+    }
+    
+    return grid[m - 1][n - 1];
 }
+
+function makeGrid(row, column) {
+    const grid = [];
+    for (let i = 0; i < row; ++i) {
+        const array = [];
+        for (let j = 0; j < column; ++j) {
+            array.push(0);
+        }
+        grid.push(array);
+    }
+    
+    return grid;
+}
+
+// Runtime: 84 ms, faster than 31.06% of JavaScript online submissions for Unique Paths.
+// Memory Usage: 36.9 MB, less than 24.19% of JavaScript online submissions for Unique Paths.
+
+// Linear Time and Space Complexity. We "traverse" the grid (M * N) once. We also take
+// that much memory making the grid to calculate possibilities.
