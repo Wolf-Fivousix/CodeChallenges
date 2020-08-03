9b29981fff9d1225384b0c7266de858def800701
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-08-03 13:41:11 -0700

    Add community solution

--- LeetCode/0098. Validate Binary Search Tree.js
+++ LeetCode/0098. Validate Binary Search Tree.js
@@ -97,3 +97,28 @@ function inOrderTraversal(root) {
 //       return true;
 //     }
 // }
+
+
+
+// Solution by jeantimex
+// This is what I was trying to do before and got lost.
+var isValidBST = function(root) {
+    if (!root) {
+        return true; // Sanity check for passing test case '[]'
+    }
+
+    function helper(root, min, max) {
+        if (!root) {
+            return true; // We hit the end of the path
+        }
+        
+        if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) {
+            return false; // current node's val doesn't satisfy the BST rules
+        }
+        
+        // Continue to scan left and right
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+    
+    return helper(root, null, null);
+};
