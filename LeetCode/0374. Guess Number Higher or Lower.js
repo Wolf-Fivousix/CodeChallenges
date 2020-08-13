a76e77711e702d2c42a43042b85af0af33b4d8c0
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-08-12 20:26:39 -0700

    Add recommended solution

--- LeetCode/0374. Guess Number Higher or Lower.js
+++ LeetCode/0374. Guess Number Higher or Lower.js
@@ -52,3 +52,47 @@ function guessNumber(n) {
 
 // Runtime: 72 ms, faster than 55.48% of JavaScript online submissions for Guess Number Higher or Lower.
 // Memory Usage: 36.9 MB, less than 5.15% of JavaScript online submissions for Guess Number Higher or Lower.
+
+// Recommended solution is to use Ternary Search. I find the extra complexity would not be worth the overhead compared to Binary Search.
+// Approach 3: Ternary Search
+// Algorithm
+
+// In Binary Search, we choose the middle element as the pivot in splitting. In Ternary Search, we choose two pivots (say m1m1 and m2m2) such that the given range is divided into three equal parts. If the required number numnum is less than m1m1 then we apply ternary search on the left segment of m1m1. If numnum lies between m1m1 and m2m2, we apply ternary search between m1m1 and m2m2. Otherwise we will search in the segment right to m2m2.
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+// public class Solution extends GuessGame {
+//     public int guessNumber(int n) {
+//         int low = 1;
+//         int high = n;
+//         while (low <= high) {
+//             int mid1 = low + (high - low) / 3;
+//             int mid2 = high - (high - low) / 3;
+//             int res1 = guess(mid1);
+//             int res2 = guess(mid2);
+//             if (res1 == 0)
+//                 return mid1;
+//             if (res2 == 0)
+//                 return mid2;
+//             else if (res1 < 0)
+//                 high = mid1 - 1;
+//             else if (res2 > 0)
+//                 low = mid2 + 1;
+//             else {
+//                 low = mid1 + 1;
+//                 high = mid2 - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// Complexity Analysis
+
+// Time complexity : O\big(\log_3 n \big)O(log 
+// 3
+// ​	
+//  n). Ternary Search is used.
+// Space complexity : O(1)O(1). No extra space is used.
