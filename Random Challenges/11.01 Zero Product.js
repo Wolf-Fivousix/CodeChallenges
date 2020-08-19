f1b81637248cef75dc2d521f6fa1cfdd958f3f31
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-08-19 12:40:31 -0700

    Add solved problem with pseudocode

---
+++ Random Challenges/11.01 Zero Product.js
@@ -0,0 +1,29 @@
+// input: [2,3,4] => 24
+// output:[12,8,6]
+
+// [2,0,4] => [0,8,0]
+// [2,5,3,0] => [0,0,0,30]
+
+// [2,6,3,4,62,0,0] => [0,0,0,0,...]
+
+
+
+// input: []
+// output: []
+// At least three elements.
+
+// Iterate through input and count the number of times that "0" appear.
+    // If zero count reaches 2, end early.
+// If there is one. do ....
+
+// Define a totalProduct at 1.
+// Iterate through the input array.
+    // for each element, we multiply the totalProduct by the current element.
+// Declare result variable.
+// Iterate through the input a second time. (Map).
+    // divide totalProduct by each element.
+    // add the result to result array.
+// return result array.
+
+// Edge Cases:
+// Overflows.
