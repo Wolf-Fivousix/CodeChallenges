d309855751c52cca3206d8bb82767c4653d91726
Wolf-Fivousix <Fivousix@gmail.com>
2021-03-17 23:33:35 -0800

    Add new problem

---
+++ Random Challenges/15.02 Storage Optimization.js
@@ -0,0 +1,35 @@
+// Storage Optimization
+// Amazon is experimenting with a flexible storage system for their warehouses. The storage
+// unit consists of a shelving system which is one meter deep with removable vertical and
+// horizontal separators. When all separators are installed, each storage space is one cubic
+// meter (1' x 1' x 1'). Determine the volume of the largest space when a series of horizontal
+// and vertical separators are removed.
+// Example
+// n = 6
+// m = 6
+// h = [4]
+// v = [2]
+// Consider the diagram below. The left image depicts the initial storage unit with n = 6
+// horizontal and m = 6 vertical separators, where the volume of the largest storage space is 1
+// × 1 x 1 . The right image depicts that unit after the fourth horizontal and second vertical
+// separators are removed. The maximum storage volume for that unit is then 2 × 2 x 1 = 4
+// cubic meters:
+// Function Description
+// Complete the function storage in the editor below.
+// storage has the following parameter(s):
+//      int n:  integer, the number of horizontal separators initially
+//      int m:  integer, the number of vertical separators initially
+//      int h[x]:  an array of integers, the horizontal separators to remove
+//      int v[y]:  an array of integers, the vertical separators to remove
+
+/*
+ * Complete the 'storage' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER_ARRAY h
+ *  4. INTEGER_ARRAY v
+ */
+
