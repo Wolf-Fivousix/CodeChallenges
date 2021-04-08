3b9d7b211fcd070a9da93e65bf32e15833e5aaa9
Wolf-Fivousix <Fivousix@gmail.com>
2021-04-07 20:05:40 -0800

    Add solution performance and proposed solution

--- LeetCode/0724. Find Pivot Index.js
+++ LeetCode/0724. Find Pivot Index.js
@@ -68,3 +68,32 @@ function createSums(array) {
     
     return result;
 }
+// Linear Time Complexity (We iterate through array twice)
+// Linear Space Complexity (We create a new array with all the sums)
+// Detail: There's a possibility for overflow of the sum array. No test case covered it, so I did not take it into account.
+// but it is a likely possibility when summing up so many values.
+
+// Runtime: 100 ms, faster than 59.58% of JavaScript online submissions for Find Pivot Index.
+// Memory Usage: 42.9 MB, less than 23.48% of JavaScript online submissions for Find Pivot Index.
+
+// This can be optmized for space by removing the sum array:
+// This is the proposed solution on the website.
+// Approach #1: Prefix Sum [Accepted]
+// Intuition and Algorithm
+
+// We need to quickly compute the sum of values to the left and the right of every index.
+
+// Let's say we knew S as the sum of the numbers, and we are at index i. If we knew the sum of numbers leftsum that are to the left of index i, then the other sum to the right of the index would just be S - nums[i] - leftsum.
+
+// As such, we only need to know about leftsum to check whether an index is a pivot index in constant time. Let's do that: as we iterate through candidate indexes i, we will maintain the correct value of leftsum.
+
+// Python
+// class Solution(object):
+//     def pivotIndex(self, nums):
+//         S = sum(nums)
+//         leftsum = 0
+//         for i, x in enumerate(nums):
+//             if leftsum == (S - leftsum - x):
+//                 return i
+//             leftsum += x
+//         return -1
