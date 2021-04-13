ab7653accd3393c1707425d6c60e37df20c2b68d
Wolf-Fivousix <Fivousix@gmail.com>
2021-04-12 18:28:26 -0800

    Add community solution

--- LeetCode/0054. Spiral Matrix.js
+++ LeetCode/0054. Spiral Matrix.js
@@ -173,3 +173,53 @@ function spiralOrder(matrix) {
 };
 // This get's ALMOST there, except that it will fail for a 1x2 matrix like [[3], [2]].
 
+// Community solution
+// By qwl5004 (Java)
+
+// public class Solution {
+//     public List<Integer> spiralOrder(int[][] matrix) {
+        
+//         List<Integer> res = new ArrayList<Integer>();
+        
+//         if (matrix.length == 0) {
+//             return res;
+//         }
+        
+//         int rowBegin = 0;
+//         int rowEnd = matrix.length-1;
+//         int colBegin = 0;
+//         int colEnd = matrix[0].length - 1;
+        
+//         while (rowBegin <= rowEnd && colBegin <= colEnd) {
+//             // Traverse Right
+//             for (int j = colBegin; j <= colEnd; j ++) {
+//                 res.add(matrix[rowBegin][j]);
+//             }
+//             rowBegin++;
+            
+//             // Traverse Down
+//             for (int j = rowBegin; j <= rowEnd; j ++) {
+//                 res.add(matrix[j][colEnd]);
+//             }
+//             colEnd--;
+            
+//             if (rowBegin <= rowEnd) {
+//                 // Traverse Left
+//                 for (int j = colEnd; j >= colBegin; j --) {
+//                     res.add(matrix[rowEnd][j]);
+//                 }
+//             }
+//             rowEnd--;
+            
+//             if (colBegin <= colEnd) {
+//                 // Traver Up
+//                 for (int j = rowEnd; j >= rowBegin; j --) {
+//                     res.add(matrix[j][colBegin]);
+//                 }
+//             }
+//             colBegin ++;
+//         }
+        
+//         return res;
+//     }
+// }
