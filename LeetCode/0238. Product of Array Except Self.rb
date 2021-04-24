6fa622a3388ec9e9be79352fb22516ca98673f97
Wolf-Fivousix <Fivousix@gmail.com>
2021-04-23 17:53:26 -0800

    Create solution with runtime

--- LeetCode/0238. Product of Array Except Self.rb
+++ LeetCode/0238. Product of Array Except Self.rb
@@ -28,3 +28,23 @@
 # Could you solve it in O(n) time complexity and without using division?
 # Could you solve it with O(1) constant space complexity? (The output array does not count as extra space for space complexity analysis.)
 
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+def product_except_self(nums)
+    # But when we have ONE zero. Every element will be zero EXCEPT the very element that is ZERO.
+    values = nums.filter { |value| value != 0 }
+    if values.length == nums.length
+        total_product = nums.inject(:*)
+        # This works for when there are NO ZEROES
+        return nums.map { |value| total_product / value }
+    elsif (values.length == nums.length - 1)
+        return nums.map { |value| value == 0 ? values.inject(:*) : 0 }
+    end
+    
+    # If there are TWO OR MORE ZEROS.
+    Array.new(nums.length).fill(0)
+end
+
+# Runtime: 128 ms, faster than 54.49% of Ruby online submissions for Product of Array Except Self.
+# Memory Usage: 217.3 MB, less than 39.52% of Ruby online submissions for Product of Array Except Self.
