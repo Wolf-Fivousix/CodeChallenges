a555eafa5fd54dc8f6d8fd74ed7b7ff384278132
Wolf-Fivousix <Fivousix@gmail.com>
2021-04-23 17:53:00 -0800

    Add Medium problem

---
+++ LeetCode/0238. Product of Array Except Self.rb
@@ -0,0 +1,30 @@
+# Medium
+
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 105
+# -30 <= nums[i] <= 30
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+# Follow up:
+
+# Could you solve it in O(n) time complexity and without using division?
+# Could you solve it with O(1) constant space complexity? (The output array does not count as extra space for space complexity analysis.)
+
