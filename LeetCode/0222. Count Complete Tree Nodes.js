091cf30e9c9348ef1d705f49ea25336b572ac7de
Wolf-Fivousix <Fivousix@gmail.com>
2024-04-29 10:23:00 -0800

    Update unsolved Easy challenge with community solution

--- LeetCode/0222. Count Complete Tree Nodes.js
+++ LeetCode/0222. Count Complete Tree Nodes.js
@@ -51,12 +51,60 @@ Sum to the previous calculation and done!
 1) Logic to find the "right most" node
     Knowing the height, we start with 1 right and X lefts (X is height - 2)
     Every time we FIND a node, we go 1 more to the right.
-    Ever time we DONT find a node, we got -1 on the right. If 0 rights, then invert the formula (1 left, X rights)
+    Ever time we DONT find a node, we got -1 on the right at the BEGINING AND do a right, right after turning left for the first time!
+    Now, the problem is... How do I code this!?
+    If 0 rights, then invert the formula (1 left, X rights)
 2) Formula to "back fill" the last level.
     Everytime we take "one right" we have HALF of the last level. So If 4 levels, 1 right = 2^4 (16) / 2 = 8 (which is the exact opposite of the formula for calculating the filled levels)
     Every next "right" will halft that amount.
     Every next "left" we count 0 and decrase the counter as if we had divided by 2.
     Once we reach the last node, we add +1 to the count (representing the last node, where the tree "ends")
+
+
+
+
+The solution is within the problem itself!
+Hear me out:
+We first find the HEIGHT of the root tree. We know for sure that this height is CORRECT. Either because the last level is filled or empty.
+If we ask ourselves: What is the height of the tree to the right/left of root. If the tree is FILLED, it SHOULD match to RootHeight - 1.
+    If it DOESN'T match though, then we found a branching path.
+    We go back one node and look for the height of the "left" branch.
+We repeat this process until we find which tree doesn't have the correct height.
+... And then how we use this info? >.< .....
+
+
+We start looking at right child, because WE KNOW the left branch is filled to the end.
+Given this new root and a height, does it match the expected height?
+    If height is 0, we're done! What do we return?
+    If it does, repeat on the right branch.
+    If it DOES NOT (meaning the node is missing), pass in the left branch.
+
+I was in the right track, but not quite seeing the crux of the problem.
+
+Solution by control_the_narrative:
+Time Complexity = O(logN * logN). Because at each step during the binary search we're traversing logN height. At end of each binary search step we're discarding half the tree.
+
+var countNodes = function(root) {
+    
+    function leftDepth(node) {
+        if(!node) return 0;
+        return leftDepth(node.left) + 1;
+    }
+    
+    function rightDepth(node) {
+        if(!node) return 0;
+        return rightDepth(node.right) + 1;
+    }
+    
+    function traverse(node) {
+        const leftLen = leftDepth(node);
+        const rightLen = rightDepth(node);
+        
+        if(leftLen === rightLen) return Math.pow(2, leftLen) - 1;
+        return traverse(node.left) + traverse(node.right) + 1;
+    }
+    return traverse(root);
+};
 
 */
 
